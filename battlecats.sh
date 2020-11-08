b1493baded50c94227f38066670b0ecfc8c110ac
MaomiHz <maomihz@gmail.com>
2020-11-08 11:52:01 -0600

    Update loadouts and schedule

--- battlecats.sh
+++ battlecats.sh
@@ -1,16 +1,23 @@
-# Siege of hippoe (95/hr+speed)
+# Siege of hippoe (96/hr+speed)
+# Date 1  4   7   10  13
+# Hour 7  11  16   8  12
+# ----------------------
+# 16  19  22  25  28  31
+# 17  13  19  20  21  22
 nohup sh cats.sh --strict-timezone -c 2 -d 3 7 6 9 10 6 7 9 10 6 7 9 10 6 7 9 10 6 7 9 10 6 7 9 10 9 10 9 10 8 9 10 9 10
+
+# Ticket Festival 11-14,21-24
 nohup sh cats.sh --fix-timezone -c 2 -d 3 7 6 9 10 6 7 9 10 6 7 9 10 6 7 9 10 6 7 9 10 6 7 9 10 9 10 9 10 8 9 10 9 10
 
 # Speed cat +$1300 (152/hr)
 nohup sh cats.sh -c 8 -d 7 7 7 7 7 7 7 6 6 6 6 6 6 6 6 6 9 9 9 9 9 9 9 9 10 8 10 8 10 8 9 10 8 10 8 10 8 10 8 9
 
-# Speed cat blitz (105/hr,8-9,12-13,19-20)
-nohup sh cats.sh -zs -c 5 -d 6 7 8 9
+# Speed cat blitz (105/hr 8,12,19)
+nohup sh cats.sh -zs -c 5 -d 6 8 9 10
 
 # Sweet XP (120/hr+speed)
-# M11-12,22-23 T8-9,19-20 W13-14,21-22
-# T12-13,20-21 F11-12,17-18
+# Mon   Tue  Wed   Thu   Fri
+# 11,22 8,19 13,21 12,20 11,17
 sh cats.sh -zs -c 2 -d 7 6 8 9 10
 
 # Meteorite stage (171/hr)
@@ -18,6 +25,9 @@ sh cats.sh -c 6 -d 6 7 9 10 9 10
 
 # Torture room
 sh cats.sh -c 18 -d 8 6 9 10
+
+# A cultured festival (123/hr Nov.6-20,2020)
+nohup sh cats.sh -c 10 -d 6 7 8 9
 
 # Slim to victory
 #sh cats.sh 8 "8 9 10 5 9 10 5 9 10 5 9 10 5 9 10 5 8 8 8 8 8 8 8 8 8 8"
